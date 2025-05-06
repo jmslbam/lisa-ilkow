84d89c9ad0c1adb330a5f52fe355f410b16a49fd
Jaime Martínez <info@jaimemartinez.nl>
2025-05-06 09:21:55 +0200

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@ module.exports = {
   content: ['./src/**/*.njk', './src/**/*.md',],
   theme: {
     extend: {},
+    fontSize: {
+      'sm': '1rem',
+      'base': '1.125rem',
+      'xl': '1.563rem',
+      '2xl': '1.563rem',
+      '3xl': '2.441rem',
+      '4xl': '3.052rem',
+      '5xl': '3.052rem',
+    }
   },
   plugins: [],
 }
